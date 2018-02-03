4a2b919f737afe24dfd39578f0e912f5c7bef12b
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-03 08:20:22 -0800

    Bump emacs-26-pretest recipe to 26.0.91

--- recipes/emacs-26-pretest.rb
+++ recipes/emacs-26-pretest.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-26-pretest' do
-  git 'http://git.savannah.gnu.org/r/emacs.git', 'emacs-26.0.90'
+  git 'http://git.savannah.gnu.org/r/emacs.git', 'emacs-26.0.91'
 
   osx do
     option '--with-ns'
